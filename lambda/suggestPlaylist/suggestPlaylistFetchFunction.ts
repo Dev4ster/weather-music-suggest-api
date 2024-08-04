434a7fd94244ccba40b17d3a7eac2273a8816c84
Victor Menezes <victormenezes35@gmail.com>
2024-08-04 14:50:43 -0300

    test: deploy test

--- lambda/suggestPlaylist/suggestPlaylistFetchFunction.ts
+++ lambda/suggestPlaylist/suggestPlaylistFetchFunction.ts
@@ -37,7 +37,7 @@ export async function handler(
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ genre, musicSuggestions }),
+      body: JSON.stringify({ genre, musicSuggestions, deploy: true }),
     };
   } catch (error) {
     console.error(error);
